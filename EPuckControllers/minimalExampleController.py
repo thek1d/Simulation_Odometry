a646ffc73ba4c1de65a6e3799ef3303e5e2eba52
Ozgur Gumuslu <thek1d@localhost.localdomain>
2020-11-21 09:13:36 +0100

    added Odometry

--- EPuckControllers/minimalExampleController.py
+++ EPuckControllers/minimalExampleController.py
@@ -35,7 +35,7 @@ def calculateWheelspeed(constants, goal, act):
     
     return vel
 
-def calculateMotorValues(goal, act, constants):
+def calculateMotorValues(goal, act, constants, odometry, wheelparameter):
     """
     TODO: include parameters
     :return: (float,float)
@@ -46,8 +46,13 @@ def calculateMotorValues(goal, act, constants):
     # TODO: calculate left and right motor velocity
     velRight = 0
     velLeft = 0
-        
-    vel = calculateWheelspeed(constants, goal, act)
+    
+    if odometry:
+        nextPoint = nextPointOdometry(act, wheelparameter)
+        vel = calculateWheelspeed(constants, goal, act=nextPoint)
+    else:
+        vel = calculateWheelspeed(constants, goal, act=act)
+    
     
     velLeft =  vel[0][0]
     velRight = vel[1][0]
@@ -55,7 +60,37 @@ def calculateMotorValues(goal, act, constants):
     return velLeft, velRight
 
 
+def nextPointOdometry(actPose, wheelparameter):
+    
+    wheelEncoderLeft.append(wheelparameter[0])
+    wheelEncoderRight.append(wheelparameter[1])
+    
+    theta = 2
+    r     = 0.04/2
+    
+    if step == 0:
+        delta_sr = delta_sl = delta_theta = 0
+        
+    else:
+        delta_sr     = r * abs(wheelEncoderRight[step] - wheelEncoderRight[step-1]) 
+        delta_sl     = r * abs(wheelEncoderLeft[step] - wheelEncoderLeft[step-1])
+        delta_theta  = (delta_sr - delta_sl) / (2*r)
+    
+    delta_s = (delta_sr + delta_sl) * .5
+    
+    diffPose = np.array([[delta_s*np.cos(actPose[theta]+delta_theta/2)],
+                         [delta_s*np.sin(actPose[theta]+delta_theta/2)],
+                         [delta_theta]])
+      
+    nextPoint = np.add(np.array([[actPose[0]],
+                                 [actPose[1]],
+                                 [actPose[2]]]), diffPose)
+    
+    return nextPoint
+
+
 def main():
+            
     robot = EPuckVRep('ePuck', port=19999, synchronous=False)
 
     robot.enableAllSensors()
@@ -63,6 +98,12 @@ def main():
     robot.setSensesAllTogether(False)  # we want fast sensing, so set robot to sensing mode where all sensors are sensed
 
     noDetectionDistance = 0.05 * robot.getS()  # maximum distance that proximity sensors of ePuck may sense
+    
+    global wheelEncoderRight, wheelEncoderLeft, step
+    
+    step              =  0
+    wheelEncoderRight = list()
+    wheelEncoderLeft  = list()
     
     #startpos = [0.5, .5, 0.0]
     goal = [1.225, 0.0, 0.0]                # wieso x so genau?
@@ -76,20 +117,27 @@ def main():
         # print( 'proximity: ', robot.getProximitySensorValues())
         # print( 'ground: ', robot.getGroundSensorValues())
         # print( 'acceleration: ', robot.getAccelerometerValues())
-        # print( 'wheel encoding: ', robot.getWheelEncoderValues())
+        print( 'wheel encoding: ', robot.getWheelEncoderValues())
         
         robot.fastSensingOverSignal()
         
         print('Pose: ',robot.getPose())
 
         # sense
-        distVector = robot.getProximitySensorValues()
+        #distVector = robot.getProximitySensorValues()
+        
+        wheelEncodingValues = robot.getWheelEncoderValues()
 
         # plan
-        leftMotor, rightMotor = calculateMotorValues(goal, robot.getPose(), constants)
+        leftMotor, rightMotor = calculateMotorValues(goal           = goal,
+                                                     act            = robot.getPose(), 
+                                                     wheelparameter = wheelEncodingValues, 
+                                                     constants      = constants,
+                                                     odometry       = True)
 
         # act
         robot.setMotorSpeeds(leftMotor, rightMotor)
+        step = step + 1
 
         time.sleep(0.05)
 
